3656ab6ffd6340eeb496aa3d4a3ed663c7c16b8d
TheSmallAnto <antd39487@gmail.com>
2026-04-04 06:05:09 -0400

    Update Emscripten.cmake

--- platforms/Emscripten.cmake
+++ platforms/Emscripten.cmake
@@ -1,3 +1,63 @@
+cmake_minimum_required(VERSION 3.10)
+
+project(RetroEngine)
+
+add_executable(RetroEngine ${RETRO_FILES})
+
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fPIC -O3")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC -O3")
+
+set(COMPILE_THEORA FALSE)
+set(THEORA_DIR dependencies/all/libtheora)
+
+add_library(libtheora STATIC
+    ${THEORA_DIR}/lib/analyze.c
+    ${THEORA_DIR}/lib/apiwrapper.c
+    ${THEORA_DIR}/lib/bitpack.c
+    ${THEORA_DIR}/lib/cpu.c
+    ${THEORA_DIR}/lib/decapiwrapper.c
+    ${THEORA_DIR}/lib/decinfo.c
+    ${THEORA_DIR}/lib/decode.c
+    ${THEORA_DIR}/lib/dequant.c
+    ${THEORA_DIR}/lib/encapiwrapper.c
+    ${THEORA_DIR}/lib/encfrag.c
+    ${THEORA_DIR}/lib/encinfo.c
+    ${THEORA_DIR}/lib/encode.c
+    ${THEORA_DIR}/lib/encoder_disabled.c
+    ${THEORA_DIR}/lib/enquant.c
+    ${THEORA_DIR}/lib/fdct.c
+    ${THEORA_DIR}/lib/fragment.c
+    ${THEORA_DIR}/lib/huffdec.c
+    ${THEORA_DIR}/lib/huffenc.c
+    ${THEORA_DIR}/lib/idct.c
+    ${THEORA_DIR}/lib/info.c
+    ${THEORA_DIR}/lib/internal.c
+    ${THEORA_DIR}/lib/mathops.c
+    ${THEORA_DIR}/lib/mcenc.c
+    ${THEORA_DIR}/lib/quant.c
+    ${THEORA_DIR}/lib/rate.c
+    ${THEORA_DIR}/lib/state.c
+    ${THEORA_DIR}/lib/tokenize.c
+)
+
+target_compile_options(libtheora PRIVATE ${THEORA_FLAGS} -sUSE_OGG=1)
+
+target_include_directories(libtheora PRIVATE ${THEORA_DIR}/include)
+target_include_directories(RetroEngine PRIVATE ${THEORA_DIR}/include)
+target_link_libraries(RetroEngine libtheora)
+
+set(EMSCRIPTEN_FLAGS
+    -sUSE_SDL=2
+    -sUSE_OGG=1
+    -sUSE_PTHREADS=1
+    -pthread
+    -DRSDK_REVISION=3
+    -DRSDK_USE_SDL2=1
+    -DRETRO_STANDALONE=1
+    -DRETRO_USE_MOD_LOADER=1
+    -DRETRO_PLATFORM=5
+)
+
 set(emsc_link_options
     # Memory
     -sINITIAL_MEMORY=536870912
@@ -47,4 +107,19 @@ set(emsc_link_options
     -Wl,--whole-archive
     ${THEORA_LIB}
     -Wl,--no-whole-archive
+)
+
+
+target_compile_options(RetroEngine PRIVATE ${EMSCRIPTEN_FLAGS})
+target_link_options(RetroEngine PRIVATE ${emsc_link_options})
+
+if(RETRO_MOD_LOADER)
+    set_target_properties(RetroEngine PROPERTIES
+        CXX_STANDARD 17
+        CXX_STANDARD_REQUIRED ON
+    )
+endif()
+
+set_target_properties(RetroEngine PROPERTIES
+    SUFFIX ".js"
 )
