86225fc03ec4c9575d8506d470f5c0c476e47ebd
TheSmallAnto <antd39487@gmail.com>
2026-04-04 20:56:59 -0400

    Update Emscripten.cmake

--- platforms/Emscripten.cmake
+++ platforms/Emscripten.cmake
@@ -2,8 +2,6 @@ cmake_minimum_required(VERSION 3.10)
 
 project(RetroEngine)
 
-set(ENV{EMCC_FORCE_STDLIBS} 1)
-
 add_executable(RetroEngine ${RETRO_FILES})
 
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fPIC -O3")
@@ -16,7 +14,6 @@ add_library(libtheora STATIC
     ${THEORA_DIR}/lib/analyze.c
     ${THEORA_DIR}/lib/apiwrapper.c
     ${THEORA_DIR}/lib/bitpack.c
-    ${THEORA_DIR}/lib/cpu.c
     ${THEORA_DIR}/lib/decapiwrapper.c
     ${THEORA_DIR}/lib/decinfo.c
     ${THEORA_DIR}/lib/decode.c
@@ -43,6 +40,7 @@ add_library(libtheora STATIC
 )
 
 target_compile_options(libtheora PRIVATE ${THEORA_FLAGS} -sUSE_OGG=1)
+target_link_libraries(libtheora PRIVATE "-sUSE_OGG=1")
 
 target_include_directories(libtheora PRIVATE ${THEORA_DIR}/include)
 target_include_directories(RetroEngine PRIVATE ${THEORA_DIR}/include)
@@ -52,7 +50,6 @@ set(EMSCRIPTEN_FLAGS
     -sUSE_SDL=2
     -sUSE_OGG=1
     -sUSE_PTHREADS=1
-    -pthread
     -DRSDK_REVISION=3
     -DRSDK_USE_SDL2=1
     -DRETRO_STANDALONE=1
@@ -61,50 +58,20 @@ set(EMSCRIPTEN_FLAGS
 )
 
 set(emsc_link_options
-    # Memory - Critical settings
-    -sINITIAL_MEMORY=536870912          # 512MB (increased from 256)
-    -sMAXIMUM_MEMORY=2147483648         # 2GB
+    -sTOTAL_MEMORY=128MB
     -sALLOW_MEMORY_GROWTH=1
-    -sSTACK_SIZE=8388608                # 8MB stack (increased)
-    
-    # Threading - Proper configuration for web workers
-    -sUSE_PTHREADS=1
-    -sPTHREAD_POOL_SIZE=navigator.hardwareConcurrency
-    -sPTHREAD_POOL_SIZE_STRICT=0
-    -sPROXY_TO_PTHREAD=0                # Don't proxy main to pthread
-    -sOFFSCREEN_FRAMEBUFFER=1           # Better canvas handling
-    
-    # Libraries
     -sUSE_SDL=2
     -sUSE_OGG=1
-    
-    # Filesystem
     -sFORCE_FILESYSTEM=1
-    -lidbfs.js
-    
-    # Module settings
     -sMAIN_MODULE=1
-    -sALLOW_TABLE_GROWTH=1 
-    -sEXIT_RUNTIME=0
-    -sENVIRONMENT=web,worker
-    -sMODULARIZE=0                      # Don't modularize
-    
-    # Error handling
-    -sASSERTIONS=1                      # Enable assertions for debugging
-    -sSTACK_OVERFLOW_CHECK=2
-    -sNO_DISABLE_EXCEPTION_CATCHING
-    
-    # Exports
-    "-sEXPORTED_RUNTIME_METHODS=['FS','ccall','cwrap','addFunction','removeFunction']"
-    "-sEXPORTED_FUNCTIONS=['_main','_RSDK_Initialize','_RSDK_Configure','_malloc','_free']"
-    
-    # Async
-    -sASYNCIFY=1
-    -sASYNCIFY_STACK_SIZE=65536
-    
-    # Other
+    -sUSE_PTHREADS=1
+    -sPTHREAD_POOL_SIZE=4
+    "-sEXPORTED_RUNTIME_METHODS=['FS','ccall','cwrap']"
+    "-sEXPORTED_FUNCTIONS=['_main','_RSDK_Initialize','_RSDK_Configure']"
     -DRSDK_REVISION=3
     -lm
+    -lidbfs.js
+    -flto
     -pthread
     -Wl,--whole-archive
     ${THEORA_LIB}
@@ -113,6 +80,9 @@ set(emsc_link_options
 
 target_compile_options(RetroEngine PRIVATE ${EMSCRIPTEN_FLAGS})
 target_link_options(RetroEngine PRIVATE ${emsc_link_options})
+
+
+set(RETRO_MOD_LOADER TRUE)
 
 if(RETRO_MOD_LOADER)
     set_target_properties(RetroEngine PROPERTIES
